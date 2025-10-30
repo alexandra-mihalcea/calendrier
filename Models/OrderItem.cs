a1a1b9932689a911f71faf62a89325b73349cd9d
Eduard Kotkas <9152439+edkotkas@users.noreply.github.com>
2025-10-30 21:53:22 +0000

    use month from rough date

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -3,14 +3,14 @@ public class OrderItem
     public int? ProductTypeId { get; set; }
     public string? Name { get; set; }
     public string? Website { get; set; }
-    public string? Url { get; set; }
+    public string? ItemUrl { get; set; }
     public string? OrderUrl { get; set; }
     public bool? Ordered { get; set; }
     public bool? Paid { get; set; }
     public bool? Received { get; set; }
-    public string? ReleaseDate { get; set; }
-    public string? RoughDate { get; set; }
-    public string? Month { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public DateTime? RoughDate { get; set; }
+    public int? Month => RoughDate.HasValue ? RoughDate.Value.Month : null;
     public string? Price { get; set; }
     public int? PaidFrom { get; set; }
 }
