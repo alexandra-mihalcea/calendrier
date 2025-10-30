39c789ef994f6fdaef7856c0d329572980a27692
Alex <alexandra-mihalcea@users.noreply.github.com>
2025-10-30 18:57:31 +0000

    homepage clock can now be customized to use differnt formats or to hide elements

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -4,4 +4,14 @@ public class AppSettings
     public string OrderDataFilePath { get; set; } = string.Empty;
     public string GoogleAPIKey { get; set; } = string.Empty;
     public bool DarkMode { get; set; }
+
+    // Home clock / date / day settings
+    public bool ClockVisible { get; set; } = true;
+    public int ClockFormat { get; set; } = 0; // index 0..9
+
+    public bool DayVisible { get; set; } = true;
+    public int DayFormat { get; set; } = 0; // index 0..9
+
+    public bool DateVisible { get; set; } = true;
+    public int DateFormat { get; set; } = 0; // index 0..9
 }
